7c80712ad6f45957f12172a235adc646c1406e07
DianaWangui <dianawanguikt048@gmail.com>
2024-01-12 16:58:01 +0300

    update

--- test_employee.py
+++ test_employee.py
@@ -2,35 +2,30 @@ import unittest
 from employee import Employee
 
 class TestEmployee(unittest.TestCase):
+  def setUp(self):
+    self.employee_1 = Employee("Diana", "Wangui", 300000)
+    self.employee_2 = Employee("John", "Kariuki", 400000)
+
   def test_email(self):
-    employee_1 = Employee("Diana", "Wangui", 300000)
-    employee_2 = Employee("John", "Kariuki", 400000)
+    self.assertEqual(self.employee_1.email, "Diana.Wangui@gmail.com")
+    self.assertEqual(self.employee_2.email, "John.Kariuki@gmail.com")
 
-    self.assertEqual(employee_1.email, "Diana.Wangui@gmail.com")
-    self.assertEqual(employee_2.email, "John.Kariuki@gmail.com")
+    self.employee_1.first_name = "Velmah"
+    self.employee_2.first_name = "Solo"
 
-    employee_1.first_name = "Velmah"
-    employee_2.first_name = "Solo"
-
-    self.assertEqual(employee_1.email, "Velmah.Wangui@gmail.com")
-    self.assertEqual(employee_2.email, "Solo.Kariuki@gmail.com")
+    self.assertEqual(self.employee_1.email, "Velmah.Wangui@gmail.com")
+    self.assertEqual(self.employee_2.email, "Solo.Kariuki@gmail.com")
 
   def test_name(self):
-    employee_1 = Employee("Diana", "Wangui", 300000)
-    employee_2 = Employee("John", "Kariuki", 400000)
-
-    self.assertEqual(employee_1.name, "Diana Wangui")
-    self.assertEqual(employee_2.name, "John Kariuki")
+    self.assertEqual(self.employee_1.name, "Diana Wangui")
+    self.assertEqual(self.employee_2.name, "John Kariuki")
 
   def test_apply_raise(self):
-    employee_1 = Employee("Diana", "Wangui", 50000)
-    employee_2 = Employee("John", "Kariuki", 60000)
+    self.employee_1.apply_raise()
+    self.employee_2.apply_raise()
 
-    employee_1.apply_raise()
-    employee_2.apply_raise()
-
-    self.assertEqual(employee_1.pay, 52500)
-    self.assertEqual(employee_2.pay, 63000)
+    self.assertEqual(self.employee_1.pay, 315000)
+    self.assertEqual(self.employee_2.pay, 420000)
 
 if __name__ == '__main__':
   unittest.main()
