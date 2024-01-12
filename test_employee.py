81ec68b4257f20b9dced2d5e195e2c9d9052221e
DianaWangui <dianawanguikt048@gmail.com>
2024-01-12 17:01:44 +0300

    update

--- test_employee.py
+++ test_employee.py
@@ -6,20 +6,32 @@ class TestEmployee(unittest.TestCase):
     self.employee_1 = Employee("Diana", "Wangui", 300000)
     self.employee_2 = Employee("John", "Kariuki", 400000)
 
+  # test case for the email method
   def test_email(self):
     self.assertEqual(self.employee_1.email, "Diana.Wangui@gmail.com")
     self.assertEqual(self.employee_2.email, "John.Kariuki@gmail.com")
 
+  # Changing employee first name
     self.employee_1.first_name = "Velmah"
     self.employee_2.first_name = "Solo"
 
     self.assertEqual(self.employee_1.email, "Velmah.Wangui@gmail.com")
     self.assertEqual(self.employee_2.email, "Solo.Kariuki@gmail.com")
 
+  # Test case for name method
   def test_name(self):
     self.assertEqual(self.employee_1.name, "Diana Wangui")
     self.assertEqual(self.employee_2.name, "John Kariuki")
 
+    # Changing employee first name
+    self.employee_1.first_name = "Velmah"
+    self.employee_2.first_name = "Solo"
+
+    self.assertEqual(self.employee_1.name, "Velmah Wangui")
+    self.assertEqual(self.employee_1.name, "Solo Wangui")
+
+
+  # test case for raise amount method for employee pay
   def test_apply_raise(self):
     self.employee_1.apply_raise()
     self.employee_2.apply_raise()
