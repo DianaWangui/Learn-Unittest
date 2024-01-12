92a2b68917469a711de1bf442a3821610ee4b8c7
DianaWangui <dianawanguikt048@gmail.com>
2024-01-12 17:05:40 +0300

    update

--- test_employee.py
+++ test_employee.py
@@ -2,9 +2,14 @@ import unittest
 from employee import Employee
 
 class TestEmployee(unittest.TestCase):
+  # This method is created before every time before running the test
   def setUp(self):
     self.employee_1 = Employee("Diana", "Wangui", 300000)
     self.employee_2 = Employee("John", "Kariuki", 400000)
+
+  def tearDown(self):
+    pass 
+
 
   # test case for the email method
   def test_email(self):
@@ -28,7 +33,7 @@ class TestEmployee(unittest.TestCase):
     self.employee_2.first_name = "Solo"
 
     self.assertEqual(self.employee_1.name, "Velmah Wangui")
-    self.assertEqual(self.employee_1.name, "Solo Wangui")
+    self.assertEqual(self.employee_2.name, "Solo Kariuki")
 
 
   # test case for raise amount method for employee pay
